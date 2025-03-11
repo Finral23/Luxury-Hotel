1cc74a203b11526e16f0f61f1a40922ad528f2ca
Finral23 <grishanov23@gmail.com>
2025-03-11 18:06:03 +0200

    Add language switch feature.

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -5,23 +5,28 @@ import {
   MenuItem,
   MenuItems,
 } from "@headlessui/react";
-import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { HiChevronDown } from "react-icons/hi";
 
 function LanguageSwitcher() {
-  const [language, setLanguage] = useState("EN");
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+    localStorage.setItem("lang", lang); // Запоминаем язык
+  };
   return (
     <>
       <Menu>
         <MenuButton className="h-6 flex items-end">
           <HiChevronDown className="pb-[1px]" />
-          {language}
+          {i18n.language.toUpperCase()}
         </MenuButton>
         <MenuItems anchor="bottom" className="pl-4">
           <MenuItem>
             <Button
               className="block text-slate-200"
-              onClick={() => setLanguage("EN")}
+              onClick={() => changeLanguage("en")}
             >
               EN
             </Button>
@@ -29,7 +34,7 @@ function LanguageSwitcher() {
           <MenuItem>
             <Button
               className="block text-slate-200"
-              onClick={() => setLanguage("RU")}
+              onClick={() => changeLanguage("ru")}
             >
               RU
             </Button>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,10 @@
 import LanguageSwitcher from "./LanguageSwitcher";
 import Logo from "./Logo";
 import { PiCallBellLight } from "react-icons/pi";
+import { useTranslation } from "react-i18next";
 
 const NavBar = () => {
+  const { t } = useTranslation();
   return (
     <div className="flex justify-between  items-center gap-2 p-5 md:p-10">
       <div className="sm:flex-1 flex justify-start">
@@ -10,9 +12,9 @@ const NavBar = () => {
       </div>
       <Logo />
       <div className="sm:flex-1 flex justify-end items-center md:gap-5">
-        <a className="invisible w-0 md:visible md:w-auto">Login</a>
+        <a className="invisible w-0 md:visible md:w-auto">{t("login")}</a>
         <button className="flex items-center justify-between gap-2 py-2 px-3 rounded-full bg-gray-950">
-          Book now <PiCallBellLight />
+          {t("bookNow")} <PiCallBellLight />
         </button>
       </div>
     </div>
