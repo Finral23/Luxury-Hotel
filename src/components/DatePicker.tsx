ef787373ab2ff9fc5bf65fa54a2ba7eeec5707ae
Finral23 <grishanov23@gmail.com>
2025-03-16 22:23:55 +0200

    Fix issue with closing date picker.

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { DateRange, Range, RangeKeyDict } from "react-date-range";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
@@ -17,6 +17,7 @@ const DatePicker = ({ onClose, onChange }: DatePickerProps) => {
   });
 
   const [isMobile, setIsMobile] = useState(false);
+  const pickerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const checkScreenSize = () => setIsMobile(window.innerWidth < 768);
@@ -24,6 +25,22 @@ const DatePicker = ({ onClose, onChange }: DatePickerProps) => {
     window.addEventListener("resize", checkScreenSize);
     return () => window.removeEventListener("resize", checkScreenSize);
   }, []);
+
+  // Закрытие при клике вне блока
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        pickerRef.current &&
+        !pickerRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose]);
 
   const handleSelect = (ranges: RangeKeyDict) => {
     const newRange = ranges.selection;
@@ -35,10 +52,9 @@ const DatePicker = ({ onClose, onChange }: DatePickerProps) => {
   };
 
   return (
-    <div
-      className={`fixed inset-0 flex items-end justify-start z-50 translate-y-[-10px]`}
-    >
+    <div className={`fixed inset-0 flex items-end justify-start z-50`}>
       <div
+        ref={pickerRef}
         className={`bg-zinc-900 text-slate-200 p-4 rounded-lg shadow-lg md:w-full max-w-2xl ${
           isMobile
             ? "w-full max-w-md translate-y-[-20px] mb-10"
